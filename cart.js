998265a8fe5a088b83ef0c410e41f0e18f86d4d0
Uche Junior Izugbo <ucjunior369@gmail.com>
2023-07-08 15:31:11 +0100

    Build the add-to-cart feature

--- cart.js
+++ cart.js
@@ -1,17 +1,35 @@
 function displayCart() {
   let cartItems = localStorage.getItem("itemsInCart");
   cartItems = JSON.parse(cartItems);
-  let itemsContainer = document.querySelector(".items");
-  if (cartItems && itemsContainer) {
-    itemsContainer.innerHTML = "";
+  let table = document.querySelector(".table");
+  let cartItem = document.querySelector(".cart-item");
+  if (cartItems && table) {
     Object.values(cartItems).map((item) => {
-      itemsContainer.innerHTML += `
-      <div class="items">
-      <i class="fa-solid fa-trash"></i>
-      <img src="${item.itemImage}">
-      <span>${item.itemTitle}</span>
+      cartItem.innerHTML += `
+      <div class="cart-row">
+      <div class="cell item">
+          <div class="item-info">
+              <div class="item-image">
+                  <img src="${item.itemImage}">
+              </div>
+              <div class="item-details">
+                  <p class="item-name">${item.itemTitle}</p>
+                  <small class="item-price">Price: $${item.itemPrice}</small>
+              </div>
+          </div>
       </div>
-      `;
+      <div class="cell">
+          <input type="number" value="${item.inCart}" min="1" class="quantity">
+      </div>
+      <div class="cell subtotal">$${item.itemPrice * item.inCart}</div>
+      <div class="cell">
+          <a class="remove-btn">Remove
+          </a>
+      </div>
+  </div>
+        `;
+      table.appendChild(cartItem);
+      localStorage.setItem("subTotal", item.itemPrice * item.inCart);
     });
   }
 }
