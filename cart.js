c51a52fb1fbf267f6a19946d5211c5a15fb764eb
ALPHA369 <ucjunior369@gmail.com>
2026-01-25 07:36:06 +0100

    Add remove item from cart functionality

--- cart.js
+++ cart.js
@@ -43,3 +43,44 @@ function displayCart() {
 }
 
 displayCart();
+
+// Remove item from cart
+function removeItem(itemImage) {
+  let cartItems = localStorage.getItem("itemsInCart");
+  cartItems = JSON.parse(cartItems);
+
+  if (cartItems && cartItems[itemImage]) {
+    let item = cartItems[itemImage];
+    let cartNumbers = parseInt(localStorage.getItem("cartNumbers"));
+    let totalCost = parseFloat(localStorage.getItem("totalCost"));
+
+    // Update cart numbers and total cost
+    localStorage.setItem("cartNumbers", cartNumbers - item.inCart);
+    localStorage.setItem("totalCost", totalCost - item.itemPrice * item.inCart);
+
+    // Remove the item from cart
+    delete cartItems[itemImage];
+    localStorage.setItem("itemsInCart", JSON.stringify(cartItems));
+
+    // Reload the page
+    location.reload();
+  }
+}
+
+// Add click event listeners to remove buttons
+function addRemoveListeners() {
+  let removeButtons = document.querySelectorAll(".remove-btn");
+  removeButtons.forEach((btn, index) => {
+    let cartItems = JSON.parse(localStorage.getItem("itemsInCart"));
+    let itemImage = Object.keys(cartItems)[index];
+
+    btn.addEventListener("click", () => {
+      removeItem(itemImage);
+    });
+  });
+}
+
+// Call the function after displaying cart
+setTimeout(() => {
+  addRemoveListeners();
+}, 100);
